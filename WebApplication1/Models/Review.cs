b0e93d36e0c0595d1cde13cf0a9859b7412eba1f
Phuc <beaver20310@gmail.com>
2024-12-02 21:11:08 +0700

    b

--- WebApplication1/Models/Review.cs
+++ WebApplication1/Models/Review.cs
@@ -11,7 +11,7 @@ namespace CcStore.Models
         public string Comment { get; set; }
 
         [BsonElement("date")]
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; } = DateTime.UtcNow;
 
         [BsonElement("reviewerName")]
         public string ReviewerName { get; set; }
